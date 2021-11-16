ad897b53b066cc48d4807ab4cb8d6be4188cf02e
Lester <sonorous.lester@gmail.com>
2021-11-16 15:36:50 +0800

    implement binary search tree - insert

--- binary-search-tree/binary_search_tree_test.go
+++ binary-search-tree/binary_search_tree_test.go
@@ -1,14 +1,43 @@
 package binary_search_tree
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
-func TestBinarySearchTree(t *testing.T) {
-
+func TestBinarySearchTree_Insert(t *testing.T) {
+	tree := &Node{data: 1}
+	tree.insert(1)
+	log.Printf("tree is: %v", tree)
 }
 
 type Node struct {
-	data int
-	left *Node
+	data  int
+	left  *Node
 	right *Node
 }
 
+func (node *Node) insert(value int) {
+	switch {
+	case value > node.data:
+		{
+			if node.right == nil {
+				node.right = &Node{data: value}
+			}else {
+				node.right.insert(value)
+			}
+		}
+	case value < node.data:
+		{
+			if node.left == nil {
+				node.left = &Node{data: value}
+			}else {
+				node.left.insert(value)
+			}
+		}
+	default:
+		{
+			log.Printf("value: %d already in the tree", value)
+		}
+	}
+}
